9ab6dc344eb0ca1045deb48056b8a0d6a462df88
Mahmoud Feshar <104142801+mahfeshar@users.noreply.github.com>
2024-05-19 16:26:45 +0000

    create id and time, when an instance is created

--- models/base_model.py
+++ models/base_model.py
@@ -10,6 +10,9 @@ class BaseModel:
     """that defines all common attributes/methods for other classes"""
     def __init__(self, *args, **kwargs):
         """Inistance for the Base class"""
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.utcnow()
+
         if kwargs:
             ft = '%Y-%m-%dT%H:%M:%S.%f'
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], ft)
@@ -17,10 +20,7 @@ class BaseModel:
             del kwargs['__class__']
             for key, value in kwargs.items():
                 setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.utcnow()
-            models.storage.new(self)
+        models.storage.new(self)
 
     def __str__(self):
         """should print: [<class name>] (<self.id>) <self.__dict__>"""
