061606b648c35462d71ebae0fa0a9ba5e8a0bdf9
Mahmoud Feshar <104142801+mahfeshar@users.noreply.github.com>
2024-05-19 18:00:19 +0000

    cleaning basemodel class

--- models/base_model.py
+++ models/base_model.py
@@ -35,7 +35,7 @@ class BaseModel:
         """Return a dictionary representation of the BaseModel instance"""
         dic = {}
         dic = self.__dict__.copy()
-        dic["__class__"] = str(type(self).__name__)
+        dic["__class__"] = type(self).__name__
         dic["created_at"] = self.created_at.isoformat()
         dic["updated_at"] = self.updated_at.isoformat()
         return dic
